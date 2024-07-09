b5295639bcec57e41c12f79d485c8069aa0c96ea
Vipin Kumar Seth <vipin.official.seth@gmail.com>
2024-07-09 17:38:43 +0530

    added movie details backdrop

--- js/script.js
+++ js/script.js
@@ -69,6 +69,8 @@ async function displayMovieDetails() {
   const movieId = window.location.search.split('=')[1];
   const movie = await fetchAPIData(`movie/${movieId}`);
 
+  //Overlay for background image
+  displayBackgroundImage('movie', movie.backdrop_path);
   const div = document.createElement('div');
   div.innerHTML = `<div class="details-top">
     <div>
@@ -108,7 +110,7 @@ async function displayMovieDetails() {
     <div class="details-bottom">
     <h2>Movie Info</h2>
     <ul>
-        <li><span class="text-secondary">Budget:</span>$${addCommasToNumber(
+        <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(
           movie.budget
         )}</li>
         <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(
@@ -125,6 +127,28 @@ async function displayMovieDetails() {
       .join(', ')}</div>
     </div>`;
   document.querySelector('#movie-details').appendChild(div);
+}
+
+//Display Backdrop on Details Pages
+function displayBackgroundImage(type, backgroundPath) {
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.backgroundImage = `url(https://image.tmbd.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.2';
+
+  if (type === 'movie') {
+    document.querySelector('#movie-details').appendChild(overlayDiv);
+  } else {
+    document.querySelector('#show-details').appendChild(overlayDiv);
+  }
 }
 
 //fetch data from TMBD
