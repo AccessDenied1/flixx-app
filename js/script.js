3cc12a70fdae1e97e82d3ff0777a3e1247be969b
Vipin Kumar Seth <vipin.official.seth@gmail.com>
2024-07-09 16:35:35 +0530

    added popular shows

--- js/script.js
+++ js/script.js
@@ -2,9 +2,62 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+async function displayPopularTvShows() {
+  const { results } = await fetchAPIData('tv/popular');
+  results.forEach((tvShow) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+          <a href="tv-details.html?id=1">
+            <img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="Show Title"
+            />
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">Show Title</h5>
+            <p class="card-text">
+              <small class="text-muted">Aired: XX/XX/XXXX</small>
+            </p>
+          </div>
+        </div>`;
+  });
+}
+
+async function displayPopularShows() {
+  const { results } = await fetchAPIData('tv/popular');
+  results.forEach((show) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = ` 
+        <a href='tv-details.html?id=${show.id}'>
+          ${
+            show.poster_path
+              ? `<img
+                src='https://image.tmdb.org/t/p/w500${show.poster_path}'
+                class='card-img-top'
+                alt='${show.name}'
+              />`
+              : `<img
+                src='images/no-image.jpg'
+                class='card-img-top'
+                alt='${show.name}'
+              />`
+          }
+        </a>
+        <div class='card-body'>
+          <h5 class='card-title'>${show.name}</h5>
+          <p class='card-text'>
+            <small class='text-muted'>Aired: ${show.first_air_date}</small>
+          </p>
+        </div>`;
+    document.querySelector('#popular-shows').appendChild(div);
+  });
+}
+
 async function displayPopularMovies() {
   const { results } = await fetchAPIData('movie/popular');
-  console.log(results);
   results.forEach((movie) => {
     const div = document.createElement('div');
     div.classList.add('card');
@@ -33,16 +86,30 @@ async function displayPopularMovies() {
     document.querySelector('#popular-movies').appendChild(div);
   });
 }
+
 //fetch data from TMBD
 async function fetchAPIData(endpoint) {
   const API_URL = 'https://api.themoviedb.org/3/';
+
+  showSpinner();
+
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=db5d851cca86bca03944d4e518af2c41&language=en-US`
   );
 
   const data = await response.json();
+
+  hideSpinner();
   return data;
 }
+
+function showSpinner() {
+  document.querySelector('.spinner').classList.add('show');
+}
+function hideSpinner() {
+  document.querySelector('.spinner').classList.remove('show');
+}
+
 //Highlight active link
 function highlightActiveLink() {
   const links = document.querySelectorAll('.nav-link');
@@ -59,10 +126,9 @@ function init() {
     case '/':
     case '/index.html':
       displayPopularMovies();
-      console.log('home');
       break;
     case '/shows.html':
-      console.log('Shows');
+      displayPopularShows();
       break;
     case '/movie-details.html':
       console.log('Movies Details');
