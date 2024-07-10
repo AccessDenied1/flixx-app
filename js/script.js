9b3a1aa44e833de954c5d4b22ba91a696c99fe71
Vipin Kumar Seth <vipin.official.seth@gmail.com>
2024-07-10 14:08:46 +0530

    added swiper slide

--- js/script.js
+++ js/script.js
@@ -210,20 +210,23 @@ async function displaySlider() {
     const div = document.createElement('div');
     div.classList.add('swiper-slide');
     div.innerHTML = `
-        <a href='movie-details.html?id=${movie.id}'>
-            <img src='https://image.tmdb.org/t/p/w500${movie.poster_path}}' alt='${movie.title}' />
+        <a href="movie-details.html?id=${movie.id}">
+          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
         </a>
-        <h4 class='swiper-rating'>
-            <i class='fas fa-star text-secondary'></i> ${movie.vote_average} / 10
-        </h4>`;
+        <h4 class="swiper-rating">
+          <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+        </h4>
+      `;
+
     document.querySelector('.swiper-wrapper').appendChild(div);
+
     initSwiper();
   });
 }
 
 function initSwiper() {
   const swiper = new Swiper('.swiper', {
-    sliderPerView: 1,
+    slidesPerView: 1,
     spaceBetween: 30,
     freeMode: true,
     loop: true,
@@ -231,15 +234,15 @@ function initSwiper() {
       delay: 4000,
       disableOnInteraction: false,
     },
-    breakpoint: {
+    breakpoints: {
       500: {
-        sliderPerView: 2,
+        slidesPerView: 2,
       },
       700: {
-        sliderPerView: 3,
+        slidesPerView: 3,
       },
       1200: {
-        sliderPerView: 4,
+        slidesPerView: 4,
       },
     },
   });
